77f84957d37dff4f73fbae56b671b3157cbceb4d
bakins <brian.akins@turner.com>
2014-02-22 06:54:03 -0500

    add some benchmark functions

--- bndm_test.go
+++ bndm_test.go
@@ -1,6 +1,7 @@
 package bndm
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -24,5 +25,41 @@ func TestSearch(t *testing.T) {
 	}
 	if subject[index] != 'n' {
 		t.Error("Search failed: character: ", subject[index])
+	}
+}
+
+func BenchmarkCompile(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		p := Compile([]byte("needle"))
+		if p == nil {
+			b.Error("Compile failed")
+		}
+	}
+}
+
+func BenchmarkSearch(b *testing.B) {
+	p := Compile([]byte("needle"))
+	if p == nil {
+		b.Error("Compile failed")
+	}
+	subject := "haystackneedlehaystack"
+
+	for i := 0; i < b.N; i++ {
+		index := p.Search([]byte(subject))
+
+		if index != 8 {
+			b.Error("Search failed: index: ", index)
+		}
+	}
+}
+
+func BenchmarkIndex(b *testing.B) {
+	subject := "haystackneedlehaystack"
+
+	for i := 0; i < b.N; i++ {
+		index := strings.Index(subject, "needle")
+		if index != 8 {
+			b.Error("Search failed: index: ", index)
+		}
 	}
 }
